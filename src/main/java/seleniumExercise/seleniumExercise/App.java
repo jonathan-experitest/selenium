db1ccf4fdb6882a3ce16d75605204d189ec6d177
jonathan.aharon <jonathan.aharon@jaharon-lt.experitest.local>
2018-02-06 11:12:11 +0200

    initial

--- src/main/java/seleniumExercise/seleniumExercise/App.java
+++ src/main/java/seleniumExercise/seleniumExercise/App.java
@@ -9,30 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	    int numOfFirefox= 3;
-    	    int numOfChrome= 3;
-    	    int numOfIE= 3;
-    	    int numOfIos= 2;
-    	    int numOfAndroid= 2;
-
-    	        for  (int a=0 ; a<numOfIE; a++) {
-    	            myTests test= new myTests(PlatformType.IE, "10", Platform.WIN8.toString(), "IE test #"+(a+1));
-    	            Thread t1= new Thread(test);
-    	            t1.start();
-    	        }
-    	        
-    	        for  (int a=0 ; a<numOfChrome; a++) {
-    	            myTests test= new myTests(PlatformType.CHROME, "56", Platform.WIN8.toString(), "Chrome test #"+(a+1));
-    	            Thread t1= new Thread(test);
-    	            t1.start();
-    	        }
-    	        for  (int a=0 ; a<numOfFirefox; a++) {
-    	            myTests test= new myTests(PlatformType.FIREFOX, "57", Platform.WIN8.toString(), "Firefox test #"+(a+1));
-    	            Thread t1= new Thread(test);
-    	            t1.start();
-    	        }
-    	        
-    	    }
+    	    
 
     }
 
+}
--- src/test/java/seleniumExercise/seleniumExercise/AppTest.java
+++ src/test/java/seleniumExercise/seleniumExercise/AppTest.java
@@ -1,5 +1,7 @@
 package seleniumExercise.seleniumExercise;
 
+import org.openqa.selenium.Platform;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -33,6 +35,28 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+    	int numOfFirefox= 3;
+	    int numOfChrome= 3;
+	    int numOfIE= 3;
+	    int numOfIos= 2;
+	    int numOfAndroid= 2;
+
+	        for  (int a=0 ; a<numOfIE; a++) {
+	            myTests test= new myTests(PlatformType.IE, "10", Platform.WIN8.toString(), "IE test #"+(a+1));
+	            Thread t1= new Thread(test);
+	            t1.start();
+	        }
+	        
+	        for  (int a=0 ; a<numOfChrome; a++) {
+	            myTests test= new myTests(PlatformType.CHROME, "56", Platform.WIN8.toString(), "Chrome test #"+(a+1));
+	            Thread t1= new Thread(test);
+	            t1.start();
+	        }
+	        for  (int a=0 ; a<numOfFirefox; a++) {
+	            myTests test= new myTests(PlatformType.FIREFOX, "57", Platform.WIN8.toString(), "Firefox test #"+(a+1));
+	            Thread t1= new Thread(test);
+	            t1.start();
+	        }
+	        
     }
 }
